11b347e80132d50ad65f1cdb3c61f01a61973d0e
redusty <ghfu2048@gmail.com>
2020-12-06 15:31:57 +0800

    删除一些不用的成员变量

--- include/Mixer.hpp
+++ include/Mixer.hpp
@@ -25,15 +25,17 @@ private:
     std::mutex mtx;
     uint32_t buffer_len;
     uint8_t *buffer;
-    uint8_t *buffer_start, *pos, *buffer_end, *end;
-    uint32_t index_start, index_pos, index_end;
-
-    // 缓存中是否还有数据
-    bool has_data;
+    uint8_t *buffer_start, *pos, *buffer_end;
 
     void mix8(uint8_t *, uint32_t);
     void mix16(uint8_t *, uint32_t);
     void mix32(uint8_t *, uint32_t);
+
+    // 需要确认wav的data到底是uint还是int
+    uint8_t mix_uint8(uint8_t, uint8_t);
+    int8_t mix_int8(int8_t, int8_t);
+    int16_t mix_int16(int16_t, int16_t);
+    int32_t mix_int32(int32_t, int32_t);
 };
 
 #endif
