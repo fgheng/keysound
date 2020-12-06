0bd1bbfb67c054ac6d31de4032e5c6feabc87019
redusty <ghfu2048@gmail.com>
2020-12-06 15:32:10 +0800

    使用ifdef来控制使用sdl 还是alsa还是pulse播放

--- include/Play.hpp
+++ include/Play.hpp
@@ -3,6 +3,19 @@
 
 #include "Mixer.hpp"
 
+// #define __USE_ALSA__
+#define __USE_PULSE__
+// #define __USE_SDL__
+// #define __SDL_CALL_BACK__
+
+#ifdef __USE_ALSA__
 extern void play(Mixer &, uint32_t fmt_size, uint16_t channels, uint32_t sample_rate, uint16_t bits_per_sample);
+#endif
+#ifdef __USE_PULSE__
+extern void play(Mixer &, uint32_t fmt_size, uint16_t channels, uint32_t sample_rate, uint16_t bits_per_sample);
+#endif
+#ifdef __USE_SDL__
+extern void play(Mixer &, uint32_t fmt_size, uint16_t channels, uint32_t sample_rate, uint16_t bits_per_sample);
+#endif
 
 #endif
