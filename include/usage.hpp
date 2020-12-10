205a375630ddd6d762f6733622f67c629edc72ba
redusty <ghfu2048@gmail.com>
2020-12-10 15:33:07 +0800

    增加参数选择

---
+++ include/usage.hpp
@@ -0,0 +1,19 @@
+#ifndef __USAGE__
+#define __USAGE__
+
+#include <iostream>
+
+static void usage()
+{
+    std::cout   << "Usage: keysound [OPTION]...\n"
+                << "Play sound while pressed keyboard keys.\n"
+                << "\n"
+                << "  -f, --file=WAV_FILE        play sound use FILE\n"
+                << "  -j, --json=JSON_FILE       config keyboard sound by json\n"
+                << "  -d, --dir=DIR              config keyboard sound by direction\n"
+                << "  -l, --log=LOG_FILE         log file\n"
+                << "  -D, --daemon               run in background\n"
+                << "  -?, -h, --help             help\n";
+}
+
+#endif
