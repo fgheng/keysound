9536d11eb867ba1d68d26f61fde4005c9b06d2b3
redusty <ghfu2048@gmail.com>
2020-12-10 14:16:10 +0800

    增加map

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,6 +1,12 @@
 #ifndef __UTILS__
 #define __UTILS__
 
+#include <string>
+
+extern void add_event_id(std::string);
+extern void del_event_id(std::string);
+extern bool event_id_exists(std::string);
+
 extern void add_event_id(int);
 extern void del_event_id(int);
 extern bool event_id_exists(int);
