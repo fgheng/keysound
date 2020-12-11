8e4d47b4f1b1c74b5fc56942005e269e37aea230
redusty <ghfu2048@gmail.com>
2020-12-11 10:25:12 +0800

    增加后台运行功能

--- src/main.cc
+++ src/main.cc
@@ -2,6 +2,7 @@
 #include "Audio.hpp"
 #include "Play.hpp"
 #include "args.hpp"
+#include <csignal>
 #include <cstdlib>
 #include <cstring>
 #include <memory>
@@ -13,8 +14,28 @@ extern "C" {
 #include <unistd.h>
 }
 
+void signal_handler(int signal) {
+    stop_detect();
+    stop_play();
+}
+
+void signal_handling() {
+    struct sigaction act = {{0}};
+    act.sa_handler = signal_handler;
+
+    sigaction(SIGHUP, &act, NULL);
+    sigaction(SIGINT, &act, NULL);
+    sigaction(SIGTERM, &act, NULL);
+    sigaction(SIGQUIT, &act, NULL);
+
+    // 避免成为僵尸进程?
+    act.sa_handler = SIG_IGN;
+    sigaction(SIGTERM, &act, NULL);
+}
+
 int main(int argc, char *argv[])
 {
+    signal_handling();
     process_command_line_arguments(argc, argv);
 
     std::string str;
@@ -33,14 +54,20 @@ int main(int argc, char *argv[])
             exit(EXIT_FAILURE);
     }
 
+    if (args.daemon) {
+        daemon(1, 0);
+    }
+
     Audio audio(str, args.flag);
     Mixer mixer(audio.get_max_len());
 
-    std::thread th(device_detect, &audio, &mixer);
-    th.detach();
-
-    play(mixer, 2048, audio.get_channels(),
+    std::thread th1(device_detect, &audio, &mixer);
+    std::thread th2(play, &mixer, 2048, audio.get_channels(),
         audio.get_sample_rate(), audio.get_bits_per_sample());
+
+    // 等待子线程结束，避免主线程提前释放资源后子线程访问失败
+    th1.join();
+    th2.join();
 
     return 0;
 }
