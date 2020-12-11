d18b8f08fca82e3699e8927213016311e2881b87
redusty <ghfu2048@gmail.com>
2020-12-11 17:41:38 +0800

    增加-k，--kill选项

--- include/usage.hpp
+++ include/usage.hpp
@@ -13,6 +13,7 @@ static void usage()
                 << "  -d, --dir=DIR              config keyboard sound by direction\n"
                 << "  -l, --log=LOG_FILE         log file\n"
                 << "  -D, --daemon               run in background\n"
+                << "  -k, --kill                 stop all\n"
                 << "  -?, -h, --help             help\n";
 }
 
