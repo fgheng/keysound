69beb72c25950e81c681d0aaaa11983f0d28095b
redusty <ghfu2048@gmail.com>
2020-12-17 21:28:10 +0800

    修复用户使用input组时，新插入键盘无权限问题

--- src/KeyDetect.cc
+++ src/KeyDetect.cc
@@ -27,6 +27,18 @@ void key_detect(std::string str_event_id, Audio *audio, Mixer *mixer) {
     fd_set fds;
     struct timeval tv;
 
+    // 如果使用suid的话，不需要sleep即可正常读取/dev/input/event文件
+    // 如果使用input组的方式，那么需要加上sleep，否则新插入的键盘会读取失败
+    // 不清楚是为什么，是因为文件创建的慢了？还是权限不够了？
+    // 也不是文件不存在的问题，我尝试判断文件是否存在，文件是存在的
+    // 那是因为权限没有跟上？尝试了一下确实是不可读，这是为什么呢？因为权限管理的延迟？
+    // 使用open的阻塞方式 O_NONBLOCK 打开也不行
+    // add_event_id(str_event_id);
+    // while (event_id_exists(str_event_id) && access((input_file_header + str_event_id).c_str(), R_OK) == -1) {
+        // usleep(100 * 1000);
+        // std::cout << "判断是否有权限" << std::endl;
+    // }
+    sleep(1);
     fd = open((input_file_header + str_event_id).c_str(), O_RDONLY);
 
     if (fd == -1) {
